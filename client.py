e02e065b7d7d5cc357e65fbfc4b1a577bf5d9e6f
AhmadJor <jorbanaahamd@gmail.com>
2022-11-17 13:25:37 +0200

    fifth commit

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 from sys import argv
 import socket 
+def legall_port(port):
+    return port > 0 and port <= 2**16 
 
 def main(argv):
-
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	ip = argv[0]
 	port = int(argv[1])
+	if not legall_port(port):
+		s.close()
 	while True:
 		string = input()
 		s.sendto(string.encode(), (ip, port))
--- server.py
+++ server.py
@@ -1,65 +1,67 @@
 import sys, socket
 
 def gtMessage(server, messages, address):
-        if len(messages) == 0:
-            server.sendto(b"", address)
+        if messages:
+            server.sendto(("\n".join(messages)).encode(), address)
             return
-        message = "\n".join(messages)
-        server.sendto(message.encode(), address)
+        server.sendto(b"", address)
+
+def legall_port(port):
+    return port > 1025 and port < 2**16 -1
+
 def exist(key,dict):
     return key in dict
+def fill_meassge(clients,messages,message,except_name):
+    for name in clients.values():
+        if name != except_name:
+            messages[name].append(message)
+
 def main(argv):
     host = ""
     port = int(sys.argv[1])
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    if not legall_port(port):
+        print("illegal port")
+        server.close()
+
     server.bind((host, port))
-    save_clients = {}
+    clients = {}
     messages = {}
 
     while True:
         data, address = server.recvfrom(1024)
-        decoded = data.decode("utf-8")
-        if len(decoded) > 1:
-            number, deco = decoded.split(" ", 1)
+        command = data.decode("utf-8")
+        if len(command) > 1:
+            number, command_string = command.split(" ", 1)
         else:
-            number = decoded
-        # print(decoded)
-        if number[0] == "1":
-            # print(save_clients)
-            joined_string = ", ".join(save_clients.values())
-            server.sendto(joined_string.encode(), address)
-            for name in save_clients.values():
-                messages[name].append(deco + " has joined")
-            save_clients[address] = deco
-            messages[deco] = []
-        elif number[0] == "2" and exist(address,save_clients):
-            for name in save_clients.values():
-                if name != save_clients[address]:
-                    messages[name].append(save_clients[address] + ": " + deco)
-            gtMessage(server, messages[save_clients[address]], address)
-            messages[save_clients[address]] = []
-        elif number[0] == "3" and exist(address,save_clients) :
-            old_name = save_clients[address]
-            save_clients[address] = deco
-            messages[deco] = messages[old_name]
+            number = command
+        
+        if number[0] == "1" and not exist(address,clients) and len(command_string)!=0:
+            server.sendto((", ".join(clients.values())).encode(), address)
+            fill_meassge(clients,messages,command_string + " has joined",command_string)
+            clients[address] = command_string
+            messages[command_string] = []
+        elif number[0] == "2" and exist(address,clients):
+            fill_meassge(clients,messages,clients[address] + ": " + command_string,clients[address])
+            gtMessage(server, messages[clients[address]], address)
+            messages[clients[address]] = []
+        elif number[0] == "3" and exist(address,clients) :
+            old_name = clients[address]
+            clients[address] = command_string
+            messages[command_string] = messages[old_name]
             messages.pop(old_name)
-            gtMessage(server, messages[save_clients[address]], address)
-            messages[save_clients[address]] = []
-
-            for name in save_clients.values():
-                if name != save_clients[address]:
-                    messages[name].append(old_name + " changed his name to " + deco)
-        #    print(messages, save_clients)
-        elif number[0] == "4" and exist(address,save_clients):
-            messages.pop(save_clients[address])
-            left = save_clients[address]
-            save_clients.pop(address)
-            for name in save_clients.values():
-                messages[name].append(left + " has left the group")
+            gtMessage(server, messages[clients[address]], address)
+            messages[clients[address]] = []
+            fill_meassge(clients,messages,old_name + " changed his name to " + command_string,clients[address])
+        elif number[0] == "4" and exist(address,clients):
+            messages.pop(clients[address])
+            left = clients[address]
+            clients.pop(address)
+            fill_meassge(clients,messages,left + " has left the group",left)
             server.sendto(b"leave", address)
-        elif number[0] == "5" and exist(address,save_clients):
-            gtMessage(server, messages[save_clients[address]], address)
-            messages[save_clients[address]] = []
+        elif number[0] == "5" and exist(address,clients):
+            gtMessage(server, messages[clients[address]], address)
+            messages[clients[address]] = []
         else:
             server.sendto(b"Illegal request", address)
 
